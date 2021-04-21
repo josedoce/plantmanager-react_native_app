c6a016738ebaa8ad4268926381309bcbe7a5b0fc
josedoce <josesamya@gmail.com>
2021-04-21 08:21:21 -0300

    dia2: criação de intermediario

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import {Welcome} from './Welcome';
+import {UserIdentification} from './UserIdentification';
+import {Confirmation} from './Confirmation';
+export {
+    Welcome,
+    UserIdentification,
+    Confirmation
+}
