1047788b219c977ebe5ad2cf11bfc8d5fd119106
josedoce <josesamya@gmail.com>
2021-04-22 23:27:14 -0300

    dia4: adição de novos components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View, Image ,StyleSheet, Platform} from 'react-native';
 import colors from '../../styles/colors';
 import fonts from '../../styles/fonts';
 import userImg from '../assets/plant.png';
 //caso use iphone-x use o react-native-iphone-x-helper para colocar uma margem top
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export function Header(){
+    const [userName, setUserName] = useState<string>();
+
+    useEffect(()=>{
+        async function loadStorageUserName(){
+            const user = await AsyncStorage.getItem('@plantmanager:user');
+            setUserName(user||'');
+        }
+        loadStorageUserName();
+    }, []);
     return (
         <View style={styles.container}>
             <View>
                 <Text style={styles.greeting}>Olá,</Text>
-                <Text style={styles.userName}>Jose</Text>
+                <Text style={styles.userName}>{userName}</Text>
             </View>
             <Image source={userImg} style={styles.image}/>
         </View>
