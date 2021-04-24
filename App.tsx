973bbf3ba2b8841bd93427fcd01f0d8358480f4f
josedoce <josesamya@gmail.com>
2021-04-23 21:51:08 -0300

    dia5: escutando notificações

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Routes from './src/routes';
+import * as Notifications from 'expo-notifications';
 
 import AppLoading from 'expo-app-loading';
 import {
@@ -7,12 +8,37 @@ import {
   Jost_400Regular,
   Jost_600SemiBold
 } from '@expo-google-fonts/jost';
+import { PlantProps } from './src/libs/storage';
 
 export default function App(){
   const [fonstLoaded] = useFonts({
     Jost_400Regular,
     Jost_600SemiBold
   });
+  useEffect(()=>{ 
+    //escutando as notificações:
+    // const subscription = Notifications.addNotificationReceivedListener(
+    //   async notification => {
+    //     const data = notification.request.content.data.plant as PlantProps;
+    //     console.log(data);
+    //   }
+    // )
+    // return () => subscription.remove();
+
+    // async function notifications(){
+    //   //retornando todos os agendamentos de notificações
+    //   // const data = await Notifications.getAllScheduledNotificationsAsync();
+    //   // console.log("------------------agendamentos-----------------");
+    //   // console.log(data);
+
+    //   //cancelar todas as notificações
+    //   const data = await Notifications.cancelAllScheduledNotificationsAsync();
+    //   console.log('---------------todas as notificações----------------')
+    //   console.log(data);
+    // }
+    // notifications();
+  },[])
+
   if(!fonstLoaded){
     return <AppLoading/>;
   }
